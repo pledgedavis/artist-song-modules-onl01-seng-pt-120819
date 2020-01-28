74393cf4c82417eb06e4fe206cbf8fcde7a71da0
Pledge Davis <pledgedavis14@gmail.com>
2020-01-28 23:20:50 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,16 @@
 module Memorable
+  module ClassMethods
   def self.reset_all
     self.all.clear
   end
 
   def self.count
     self.all.count
+  end
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
   end
 
 end
